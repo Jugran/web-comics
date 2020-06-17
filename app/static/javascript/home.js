d75a01c192fdf425c957ad1dd28557d7d69dc173
Samarth Jugran <jugransamarth@gmail.com>
2020-06-17 20:10:05 +0530

    add active link highlight

---
+++ app/static/javascript/home.js
@@ -0,0 +1,26 @@
+
+function setActiveNavLink(){
+    // console.log('setting active item');
+    var currentActive = document.getElementsByClassName('active');
+
+    if (currentActive.length > 0 ){
+        currentActive[0].classList.remove('active');
+    }
+
+    var path = window.location.pathname;
+
+    if (path === '/'){
+        // homepage
+        document.getElementById('home').classList.add('active');
+    }
+    else{
+        document.getElementById(path.substr(1)).classList.add('active');
+    }
+}
+
+
+// function setActiveItem(){
+//     var currentActive = document.getElementsByClassName('active');
+//     currentActive[0].classList.remove('active');
+//     this.classList.add('active');
+// }
