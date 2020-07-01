edac1810330415b78615b8b82d96e1824e2a0e3f
Samarth Jugran <jugransamarth@gmail.com>
2020-07-01 19:37:11 +0530

    add comic info card

--- app/static/javascript/viewer.js
+++ app/static/javascript/viewer.js
@@ -3,23 +3,23 @@ var modal = document.getElementById('image-viewer');
 var imgs = document.getElementsByClassName('card-img-top');
 
 var modalImage = document.getElementById('modal-image');
-var caption = document.getElementById('caption');
 
-for (img of imgs){
-    img.onclick = function(){
-        console.log('open image modal');
-        
-        // disable page scrolling
-        document.getElementsByTagName('html')[0].classList.add('overflow-hidden');
-       
-        //make modal visible
-        modal.classList.add('d-flex');
-        modalImage.src = this.src;
-        // caption.innerText = this.alt;
+function openModal(img){
+    console.log('open image modal');
 
-        // enable blur
-        document.getElementById('comic-container').classList.add('blur');
-    }
+    // disable page scrolling
+    document.getElementsByTagName('html')[0].classList.add('overflow-hidden');
+   
+    //make modal visible
+    modal.classList.add('d-flex');
+    modalImage.src = img.src;
+    // caption.innerText = this.alt;
+
+    // enable blur
+    document.getElementById('comic-container').classList.add('blur');
+
+    // set card info
+    setImageCardInfo(img); 
 }
 
 // close function
@@ -33,9 +33,23 @@ function closeModal(){
     document.getElementById('comic-container').classList.remove('blur');
 }
 
-document.getElementById('close-button').onclick = closeModal;
-document.getElementById('modal-background').onclick = closeModal;
+// document.getElementById('close-button').onclick = closeModal;
+// document.getElementById('modal-background').onclick = closeModal;
 
+
+// Info card fucntion
+function setImageCardInfo(img){
+    var comicName = document.getElementById('comic-name');
+    var comicTitle = document.getElementById('comic-title');
+    var comicDesc = document.getElementById('comic-description');
+
+    // TODO: change this hack to proper REST api alternative
+    // comicName.innerText = this.src.split('/')[2].split('.')[0];
+    // var info = this.alt.split('|');
+    comicName.innerText = img.alt;
+    // comicDesc.innerText = info[1];
+
+}
 
 // var close = document.getElementById('close-button');
 
